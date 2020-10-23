8b0fe5a8f5a9a0958e9000fe1d144ea48aabc351
xjw <1521975316@qq.com>
2020-10-23 14:27:20 +0800

    init

---
+++ android/app/src/main/java/com/xjw/wanwan/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.xjw.wanwan
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import com.xjw.wanwan.rct.MainRCTActivity
+
+class MainActivity : AppCompatActivity() {
+
+  private val requestCodeForPermission: Int = 0
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    checkDrawOverlaysPermission()
+    arrayListOf<Int>(R.id.btn_main_open_rct_page).forEach {
+      findViewById<View>(it).setOnClickListener {
+        startActivity(Intent(this, MainRCTActivity::class.java))
+      }
+    }
+  }
+
+  /**
+   * rct 需要悬浮窗权限来展示开发中的错误信息
+   */
+  private fun checkDrawOverlaysPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      if (!Settings.canDrawOverlays(this)) {
+        startActivityForResult(
+          Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName")),
+          requestCodeForPermission)
+      }
+    }
+
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == requestCodeForPermission) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (!Settings.canDrawOverlays(this)) {
+          println(">> 获取悬浮窗权限失败")
+        }
+      }
+    }
+  }
+
+}
---
+++ android/app/src/main/java/com/xjw/wanwan/rct/MainRCTActivity.kt
@@ -0,0 +1,72 @@
+package com.xjw.wanwan.rct
+
+import android.os.Bundle
+import android.view.KeyEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
+import com.xjw.wanwan.BuildConfig
+
+/**
+ * Created by xjw on 2020/10/22 15:21
+ */
+class MainRCTActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+
+  private val rctInstanceManager: ReactInstanceManager by lazy {
+    ReactInstanceManager.builder()
+      .setApplication(application)
+      .setCurrentActivity(this)
+      .setBundleAssetName("index.android.bundle")
+      .setJSMainModulePath("index")
+      .addPackage(MainReactPackage())
+      .setUseDeveloperSupport(BuildConfig.DEBUG)
+      .setInitialLifecycleState(LifecycleState.RESUMED)
+      .build()
+  }
+
+  private val rctRoot: ReactRootView by lazy {
+    ReactRootView(this)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    rctRoot.startReactApplication(rctInstanceManager, "wanwanRCTApp", null)
+    setContentView(rctRoot)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    rctInstanceManager.onHostResume(this)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    rctInstanceManager.onHostPause(this)
+  }
+
+  override fun onDestroy() {
+    rctInstanceManager.onHostDestroy(this)
+    super.onDestroy()
+  }
+
+  override fun invokeDefaultOnBackPressed() {
+    super.onBackPressed()
+  }
+
+  override fun onBackPressed() {
+    rctInstanceManager.onBackPressed()
+  }
+
+  override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+    if (keyCode == KeyEvent.KEYCODE_MENU) {
+      rctInstanceManager.showDevOptionsDialog()
+      return true
+    }
+    return super.onKeyUp(keyCode, event)
+  }
+
+}
